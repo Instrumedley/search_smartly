035f9ef8d3b7c0b1f1a7aea0604b60ad42fe35cc
Instrumedley <rafaelalfredo2008@gmail.com>
2024-02-21 16:56:58 +0100

    Final changes on admin to reflect assignment requirements

--- pois/admin.py
+++ pois/admin.py
@@ -6,8 +6,9 @@ from .models import PointOfInterest
 @admin.register(PointOfInterest)
 class PointOfInterestAdmin(admin.ModelAdmin):
     list_display = ('id', 'external_id', 'name', 'category', 'get_average_rating')
-    search_fields = ('external_id', 'name')
+    search_fields = ('id', 'external_id', 'name')
     readonly_fields = ('external_id',)
+    list_filter = ('category',)
 
     def get_average_rating(self, obj):
         if obj.ratings:
